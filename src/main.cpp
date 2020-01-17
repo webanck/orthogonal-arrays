49012d7200dffb3c26033ae83a6764a406265134
Antoine Webanck <antoine.webanck@gmail.com>
2020-01-17 18:58:40 +0100

    Fixing the generator instanciation.
    Passing parameters to the constructor of the different OA Samplers
    is not enough.
    The parameters have to be set up manually using accessors or the
    generated samples are garbage.

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,27 @@
 
 #include <OACMJND.h>
 #include <OABose.h>
+#include <OABush.h>
 
 
 using namespace std;
 
+enum OA_TYPE : uint
+{
+	BOSE = 0,
+	BUSH,
+	CMJND
+};
+
 int main(int argc, char ** argv)
 {
-	const string usage("Usage:-m <realisations> -d <dimensions> -n <samples> -o <path>");
+	const string usage("Usage:-m <realisations> -d <dimensions> -n <samples> -o <path> [--bose | --bush | --cmjnd]");
 
 	uint realisations_number(1u);
 	uint dimensions_number(1u);
 	uint samples_number(1u);
 	string output_path("out.dat");
+	OA_TYPE oa_type(BOSE);
 
 	for(int i(1); i < argc; i++)
 	{
@@ -45,6 +54,12 @@ int main(int argc, char ** argv)
 			assert(argc > ++i);
 			output_path = string(argv[i]);
 		}
+		else if(argument.compare("--bose") == 0)
+			oa_type = BOSE;
+		else if(argument.compare("--bush") == 0)
+			oa_type = BUSH;
+		else if(argument.compare("--cmjnd") == 0)
+			oa_type = CMJND;
 		else
 		{
 			cout << usage << endl;
@@ -66,19 +81,43 @@ int main(int argc, char ** argv)
 	//const float max_jitter(1.);
 	//CMJNDInPlace sampler(samples_number, dimensions_number, ot, randomized, max_jitter);
 
-	const OffsetType ot(CMJ_STYLE);
-	//const OffsetType ot(MJ_STYLE);
+	//const OffsetType ot(CMJ_STYLE);
+	const OffsetType ot(MJ_STYLE);
 	const bool randomized(true);
 	const float max_jitter(1.);
-	BoseOAInPlace sampler(samples_number, ot, randomized, max_jitter, dimensions_number);
-	
+	OrthogonalArray * pSampler(nullptr);
+	switch(oa_type)
+	{
+		case BOSE:
+			pSampler = new BoseOAInPlace(samples_number, ot, randomized, max_jitter, dimensions_number);
+			break;
+		case BUSH:
+			pSampler = new BushOAInPlace(samples_number, 2, ot, randomized, max_jitter, dimensions_number);
+			break;
+		case CMJND:
+			pSampler = new CMJNDInPlace(samples_number, dimensions_number, ot, randomized, max_jitter);
+			break;
+	}
+
+	pSampler->setRandomized(randomized);
+	pSampler->setDimensions(dimensions_number);
+	pSampler->setNumSamples(samples_number);
+	pSampler->setJitter(max_jitter);
+
+	cout << "Sample name:" << pSampler->name() << endl;
+	cout << "Strength:" << pSampler->strength() << endl;
+	cout << "Randomized:" << (pSampler->randomized() ? "yes" : "no") << endl;
+	cout << "Jitter:" << pSampler->jitter() << endl;
+	cout << "Offset type:" << pSampler->offsetTypeNames()[pSampler->offsetType()] << endl;
+
+
 	float * sample(new float[dimensions_number]);
 	for(uint r(0u); r < realisations_number; r++)
 	{
 		for(uint s(0u); s < samples_number; s++)
 		{
-			sampler.sample(sample, s);
-			//sampler.sample(sample, r*dimensions_number*samples_number + s);
+			pSampler->sample(sample, s);
+			//sampler->sample(sample, r*dimensions_number*samples_number + s);
 			for(uint d(0u); d < dimensions_number; d++)
 			{
 				if(d > 0u) file << " ";
@@ -87,9 +126,10 @@ int main(int argc, char ** argv)
 			file << endl;
 		}
 		file << "#" << endl;
-		sampler.setRandomized(true);
+		pSampler->setRandomized(true);
 	}
 	delete[] sample;
+	delete pSampler;
 	file.close();
 
 	return 0;
